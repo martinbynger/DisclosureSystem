d0a4db53c5c36e385e338820ccb1138827448bc3
Martin Bynger <martinbynger@users.noreply.github.com>
2018-01-05 13:08:38 +0100

    First Upload

---
+++ Disclosure Libraries/Glenshield.Cryptography/Glenshield.Cryptography/CryptographyService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glenshield.Cryptography
+{
+    public static class CryptographyService
+    {
+
+        public static string Encrypt(string dataToEncrypt)
+        {
+            byte[] dataToEncryptAsBytes = UnicodeEncoding.Unicode.GetBytes(dataToEncrypt);
+
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UnicodeEncoding.Unicode.GetBytes(MakeDynamicCertificate()));
+
+                using (TripleDESCryptoServiceProvider tripleDESCryptoServiceProvider = new TripleDESCryptoServiceProvider()
+                {
+                    Key = keys,
+                    Mode = CipherMode.ECB,
+                    Padding = PaddingMode.PKCS7
+                })
+                {
+                    ICryptoTransform cryptoTransform = tripleDESCryptoServiceProvider.CreateEncryptor();
+                    byte[] encryptedData = cryptoTransform.TransformFinalBlock(dataToEncryptAsBytes, 0, dataToEncryptAsBytes.Length);
+
+                    return Convert.ToBase64String(encryptedData, 0, encryptedData.Length);
+                }
+
+            }
+        }
+
+        public static string Decrypt(string dataToDecrypt)
+        {
+            byte[] dataToDencryptAsBytes = Convert.FromBase64String(dataToDecrypt);
+
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UnicodeEncoding.Unicode.GetBytes(MakeDynamicCertificate()));
+
+                using (TripleDESCryptoServiceProvider tripleDESCryptoServiceProvider = new TripleDESCryptoServiceProvider()
+                {
+                    Key = keys,
+                    Mode = CipherMode.ECB,
+                    Padding = PaddingMode.PKCS7
+                })
+                {
+                    ICryptoTransform cryptoTransform = tripleDESCryptoServiceProvider.CreateDecryptor();
+                    byte[] decryptedData = cryptoTransform.TransformFinalBlock(dataToDencryptAsBytes, 0, dataToDencryptAsBytes.Length);
+
+                    return UnicodeEncoding.Unicode.GetString(decryptedData);
+                }
+
+            }
+        }
+
+        public static string MakeDynamicCertificate()
+        {
+            string CertificateBase = "FHJ(/&%¤REfvD23yNMK)7hBT¤";
+            int DynamicPartOfCertificate = (DateTime.Now.Day * 3);
+            return DynamicPartOfCertificate.ToString() + CertificateBase;
+        }
+    }
+}
